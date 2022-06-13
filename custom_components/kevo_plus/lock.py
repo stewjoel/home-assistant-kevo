69e25c3a7874642eeeaa646008a5d32ab3f92bf5
dcmeglio <dmeglio@gmail.com>
2022-06-12 20:24:34 -0400

    More reworking on retries

--- custom_components/kevo_plus/lock.py
+++ custom_components/kevo_plus/lock.py
@@ -49,9 +49,9 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         max_retries = lock[CONF_MAX_RETRIES]
         retry_delay = lock[CONF_RETRY_DELAY]
         
+		kevo = None
         for attempt in range(max_retries):
             try:
-                kevo = None
                 if kevo is None:
                     kevo = KevoLock.FromLockID(lock_id, email, password)
                 else:
